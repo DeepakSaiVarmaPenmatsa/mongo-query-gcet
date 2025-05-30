e66f4b14b54bfcda7942b365b2ac7a3797489294
DeepakSaiVarmaPenmatsa <pdsvarma4@gmail.com>
2025-05-30 19:25:51 +0530

    up

---
+++ q11.js
@@ -0,0 +1,22 @@
+db.employees.aggregate([
+    {$sort : { salary : 1 }},
+    {$limit:3},
+    {$project :{
+        name:1,
+        salary:1,
+        brand:{
+           $cond: {
+             if: {$gt: ["$age",40]},
+             then:"Brand A",
+             else:"Brand B",
+           },
+        },
+    },
+},
+    {$project :{
+        name:1,
+        monthlySalary: "$salary",
+        annualSalary: {$multiply:["$salary",12]}
+    }}
+
+]);
--- q3.js
+++ q3.js
@@ -1,19 +1,22 @@
-db.createCollections("products")
+db.createCollection("products");
+
 db.products.insertMany([
-    {
-    name:"Product1",
-    price: 250,
-    img:"https://picsum.photos/200/300?grayscale",
-    },
-    {
-     name:"Product1",
-    price: 200,
-    img:"https://picsum.photos/200/300?grayscale",   
-    },
-    {
-    name:"Product3",
-    price: 300,
-    img:"https://picsum.photos/200/300?grayscale",
-    },
+  {
+    name: "Apple",
+    description: "Fresh and juicy red apple.",
+    imgUrl: "https://example.com/images/apple.jpg",
+    price: 1.5
+  },
+  {
+    name: "Banana",
+    description: "Ripe yellow bananas full of energy.",
+    imgUrl: "https://example.com/images/banana.jpg",
+    price: 0.75
+  },
+  {
+    name: "Orange",
+    description: "Citrus fruit rich in Vitamin C.",
+    imgUrl: "https://example.com/images/orange.jpg",
+    price: 1.2
+  }
 ]);
-db.products.find();
