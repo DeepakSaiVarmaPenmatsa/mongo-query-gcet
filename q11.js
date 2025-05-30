393102a6c92f08f281089732d1bf8b9ccf8ad05b
DeepakSaiVarmaPenmatsa <pdsvarma4@gmail.com>
2025-05-30 19:28:24 +0530

    up

--- q11.js
+++ q11.js
@@ -1,6 +1,8 @@
 db.employees.aggregate([
-    {$sort : { salary : 1 }},
+    {$sort : { salary : -1 }},
     {$limit:3},
+
+
     {$project :{
         name:1,
         salary:1,
@@ -9,12 +11,15 @@ db.employees.aggregate([
              if: {$gt: ["$age",40]},
              then:"Brand A",
              else:"Brand B",
-           },
-        },
-    },
+           }
+        }
+    }
 },
+
+
     {$project :{
         name:1,
+        email:1,
         monthlySalary: "$salary",
         annualSalary: {$multiply:["$salary",12]}
     }}
